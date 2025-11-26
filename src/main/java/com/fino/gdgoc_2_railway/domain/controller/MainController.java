1027cc231f6667cadce10ca61e3713f36c248cf1
karl21-02 <manuna530@gmail.com>
2025-11-26 16:22:24 +0900

    feat: 사진 저장소

--- src/main/java/com/fino/gdgoc_2_railway/domain/controller/MainController.java
+++ src/main/java/com/fino/gdgoc_2_railway/domain/controller/MainController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "강원대 컴공 재학중인 최태영은 집을 개가고 싶지만, 갈 수 없어 강연을 듣고 있다. 하지만, 그의 노트북엔 알 수 없는 정보가 보이는데...! 맞추면 100만원";
+    @RequestMapping("/api/health")
+    public String health() {
+        return "OK";
     }
 }
